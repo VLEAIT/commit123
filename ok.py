ac8a20fad0076d3443bac6ab04ef5ddae52f5824
vision <visionthapa8540@gmail.com>
2026-01-23 09:25:07 +0545

    function chapter 4 start

--- ok.py
+++ ok.py
@@ -9,3 +9,16 @@ print(10>5>2)
 # try and except is used to like if else and each time peoplo use it is run program and see if it crashes and if then use another one
 # if even one line of a block in try doesnot run then other statement also doesnot run
 # ctrl + shift  + p and then type toggle suggest and click
+
+
+# funtion chapter 4
+
+def vision():
+    print("helkko")
+    print("reuse")
+
+vision()
+print('hello')
+vision()
+
+# def as in used to declare a function and vision is user defined name given to that function and it can be used to reuse code miltiple times 

97262df9b4d4f4f081ad22943fbd57fd4db9003b
vision <visionthapa8540@gmail.com>
2026-01-31 16:46:30 +0545

    npk input module

--- Npkcalci.py
+++ Npkcalci.py
@@ -11,3 +11,27 @@ class Soil_calci:
            print(" n =",N,"High")
         else:
            print("enter correct value")
+        
+        if P < 15:
+           self.Nutrient_P = 1
+           print(" p =",P,"Low")
+        elif P > 15 or P <40:
+           self.Nutrient_P = 2
+           print(" p =",P,"Medium")
+        elif P > 40:
+           self.Nutrient_P =3
+           print(" p =",P,"High")
+        else:
+           print("enter correct value")
+
+        if K  < 80:
+           self.Nutrient_K = 1
+           print(" k =",K,"low")
+        elif K >80 or K <200:
+           self.Nutrient_K = 2
+           print(" k =",K,"Medium")
+        elif K > 200:
+           self.Nutrient_K =3
+           print(" k =",K,"High")
+        else:
+           print( "enter correct value")    

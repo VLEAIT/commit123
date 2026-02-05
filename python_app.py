f668a9b4a786898fa19a031ed651adb617a15b9f
vision <visionthapa8540@gmail.com>
2026-02-05 20:45:34 +0545

    complete

--- python_app.py
+++ python_app.py
@@ -19,3 +19,10 @@ em['From'] = email_sender
 em["To"] = email_receiver
 em["subject"] =subject
 em.set_content(body)
+
+context = ssl.create_default_context
+
+with smtplib.STMP_SSL('smtp.gmail.com', 465, context =context) as smtp:
+    smtp.login(email_sender,email_receiver)
+    smtp.sendmail(email_sender,email_receiver,em.as_string())
+    
